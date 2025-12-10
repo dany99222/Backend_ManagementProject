d203b6e5e31f4f5da4e157fdd9205faba2aa9aa3
dany99222 <102881074+dany99222@users.noreply.github.com>
2025-12-10 13:19:16 -0800

    Relacionando los modelos de Proyects and Task

--- src/models/Proyect.ts
+++ src/models/Proyect.ts
@@ -1,31 +1,45 @@
-
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+import { InterfaceTask } from "./Task";
 
 // type-modelo de typescript
-export interface InterfaceProyect extends Document{
+export interface InterfaceProyect extends Document {
   projectName: string;
   clientName: string;
   description: string;
-};
+  tasks: PopulatedDoc<InterfaceTask & Document>[]; // un proyecto mcuhas tareas
+}
 
 // type-modelo para mongoose
-const ProyectSchema: Schema = new Schema({
-  projectName: {
-    type: String,
-    required: true,
-    trim: true,
+const ProyectSchema: Schema = new Schema(
+  {
+    projectName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    clientName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tasks: [
+      //Un proyecto muchas atreas
+      {
+        type: Types.ObjectId,
+        ref: "Task",
+      },
+    ],
   },
-  clientName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+  {
+    //crea dos campos para almacenar fecha de creacion y actualizacion
+    timestamps: true,
+  }
+);
 
 //Conectas el modelo con mongoose
 const Proyect = mongoose.model<InterfaceProyect>("Proyect", ProyectSchema);
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,24 +1,36 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 
 // type-modelo de typescript
 export interface InterfaceTask extends Document {
   name: string;
   description: string;
+  project: Types.ObjectId; //Una tarea a un proyecto
 }
 
 //Modelo de mongose
-export const TaskSchema: Schema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
+export const TaskSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    project: {
+      //Una tarea a un proyecto
+      type: Types.ObjectId,
+      ref: "Project",
+    },
   },
-  description: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-});
+  {
+    //crea dos campos para almacenar fecha de creacion y actualizacion
+    timestamps: true,
+  }
+);
 
 const Task = mongoose.model<InterfaceTask>("Task", TaskSchema);
 export default Task;
