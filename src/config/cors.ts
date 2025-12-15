cb373f4728ba8343ee81a5629608560d11c20937
dany99222 <102881074+dany99222@users.noreply.github.com>
2025-12-15 15:49:52 -0800

    Habilitando cors para comunicar frontend con backend

---
+++ src/config/cors.ts
@@ -0,0 +1,14 @@
+import { CorsOptions } from "cors";
+
+// Nos sirve para permitir que nuestra url dele frontend  conecte con el backend
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    const whitelist = [process.env.FRONTEND_URL];
+
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 // Creamos la logica del servidor
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors'
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config(); // carga las variables del archivo .env
 connectDB(); //conexion hacia la base de datos
 const app = express(); // incia la aplicacion de express
+app.use(cors(corsConfig)) //Conectamos el frontend con el backend
 
 app.use(express.json()); //convierte  las solicitudes https en json en un objeto js
 // Routers
