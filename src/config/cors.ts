a1e5f12283ab6e57990ae1a86f2b0f40978e8dfc
dany99222 <102881074+dany99222@users.noreply.github.com>
2025-12-21 14:26:14 -0800

    Se creo el router para auth

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,7 @@ import { CorsOptions } from "cors";
 // Nos sirve para permitir que nuestra url dele frontend  conecte con el backend
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
+    console.log(origin)
     const whitelist = [process.env.FRONTEND_URL];
 
     if (whitelist.includes(origin)) {
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,26 @@
 // Creamos la logica del servidor
 import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors'
+import cors from "cors";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import authRoutes from "./routes/authRoutes";
 import morgan from "morgan";
 
 dotenv.config(); // carga las variables del archivo .env
 connectDB(); //conexion hacia la base de datos
 const app = express(); // incia la aplicacion de express
-app.use(cors(corsConfig)) //Conectamos el frontend con el backend
+// app.use(cors(corsConfig)); //Conectamos el frontend con el backend
 
 //Para ver que ruta estamos utilizando
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 app.use(express.json()); //convierte  las solicitudes https en json en un objeto js
 
 // Routers
 // (arrupa diferentes grupos de rutas )
+app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
 export default app;
