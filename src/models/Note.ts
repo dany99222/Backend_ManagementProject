a40912d11447dc771a8d4f086ff67df5ed9a83bc
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-01-24 09:37:17 -0800

    Se creo el modelo para los comentarios de las Task

---
+++ src/models/Note.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+
+// Type script
+export interface IntefaceNote extends Document {
+  content: string;
+  createdBy: Types.ObjectId;
+  task: Types.ObjectId;
+}
+
+const NoteSchema: Schema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    Task: {
+      type: Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Note = mongoose.model<IntefaceNote>("note", NoteSchema);
