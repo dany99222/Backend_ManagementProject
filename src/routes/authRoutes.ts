e11263c9e3dbfa5700c672eea657cb555463fada
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-01-27 12:09:43 -0800

    Creando una funcion que revise el password del usuario autenticado

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -111,4 +111,10 @@ router.post(
   AuthController.updateCurrentUserPassword
 );
 
+router.post('/check-password',
+  authenticate,
+  body("password").notEmpty().withMessage("El password no puede ir vacio"),
+  handleInputErrors,
+  AuthController.checkPassword
+)
 export default router;
