cc0ce1af7e1586fd168b498012711d41b212bda3
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-01-29 14:22:43 -0800

    Finalizando ultimos ajustes

--- src/models/Proyect.ts
+++ src/models/Proyect.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
-import { InterfaceTask } from "./Task";
+import Task, { InterfaceTask } from "./Task";
 import { InterfaceUser } from "./User";
+import Note from "./Note";
 
 // type-modelo de typescript
 export interface InterfaceProyect extends Document {
@@ -8,8 +9,8 @@ export interface InterfaceProyect extends Document {
   clientName: string;
   description: string;
   tasks: PopulatedDoc<InterfaceTask & Document>[]; // un proyecto mcuhas tareas
-manager: PopulatedDoc<InterfaceUser & Document>
-team: PopulatedDoc<InterfaceUser & Document>[]
+  manager: PopulatedDoc<InterfaceUser & Document>;
+  team: PopulatedDoc<InterfaceUser & Document>[];
 }
 
 // type-modelo para mongoose
@@ -39,22 +40,34 @@ const ProyectSchema: Schema = new Schema(
     ],
     manager: {
       type: Types.ObjectId,
-        ref: "User",
+      ref: "User",
     },
-     team: [
+    team: [
       //Un proyecto muchas atreas
       {
         type: Types.ObjectId,
         ref: "User",
-    
       },
     ],
   },
   {
     //crea dos campos para almacenar fecha de creacion y actualizacion
     timestamps: true,
-  }
+  },
 );
+
+// Middlewares
+ProyectSchema.pre("deleteOne", { document: true }, async function () {
+  const projectId = this._id;
+  if (!projectId) return;
+
+  const tasks = await Task.find({ project: projectId });
+
+  for(const task of tasks) {
+    await Note.deleteMany({ task: task._id });
+  }
+  await Note.deleteMany({ project: projectId });
+});
 
 //Conectas el modelo con mongoose
 const Proyect = mongoose.model<InterfaceProyect>("Proyect", ProyectSchema);
