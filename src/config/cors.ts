59b6f2b697c8b4e7977cd2b7b5ce6000da139ef4
dany99222 <102881074+dany99222@users.noreply.github.com>
2026-01-30 08:38:42 -0800

    fix

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,22 +1,19 @@
-import { CorsOptions } from "cors";
-
-// Nos sirve para permitir que nuestra url dele frontend  conecte con el backend
+import { CorsOptions } from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+ 
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
-    
-    const whitelist = [process.env.FRONTEND_URL];
-
-if(process.argv[2] === '--api'){
-  whitelist.push(undefined)
-}
-
-    if (whitelist.includes(origin)) {
+    const whiteList = [process.env.FRONTEND_URL];
+ 
+    if (process.argv[2] == '--api') {
+      whiteList.push(undefined);
+    }
+    if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Error de CORS"));
+      callback(new Error('Error de CORS'));
     }
   },
 };
  
-
-console.log('h')
